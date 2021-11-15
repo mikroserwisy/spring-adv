aac93fdb22b3cee67405a675a8319020f4014e38
landrzejewski <landrzejewski.poczta@gmail.com>
2021-11-15 14:01:54 +0100

    init

---
+++ shop/src/main/java/pl/training/shop/payments/rest/GetPaymentUseCaseRestAdapter.java
@@ -0,0 +1,28 @@
+package pl.training.shop.payments.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.training.shop.commons.streotype.RestAdapter;
+import pl.training.shop.payments.GetPaymentUseCase;
+
+import static lombok.AccessLevel.PACKAGE;
+
+@RequestMapping("payments")
+@RestAdapter
+@RequiredArgsConstructor(access = PACKAGE)
+class GetPaymentUseCaseRestAdapter {
+
+    private final PaymentsRestMapper mapper;
+    private final GetPaymentUseCase getPaymentUseCase;
+
+    @GetMapping("{id}")
+    public ResponseEntity<PaymentDto> getById(@PathVariable String id) {
+        var payment = getPaymentUseCase.getById(id);
+        var paymentDto = mapper.toDto(payment);
+        return ResponseEntity.ok(paymentDto);
+    }
+
+}
--- shop/src/main/java/pl/training/shop/payments/rest/ProcessPaymentUseCaseRestAdapter.java
+++ shop/src/main/java/pl/training/shop/payments/rest/ProcessPaymentUseCaseRestAdapter.java
@@ -5,10 +5,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.training.shop.commons.rest.UriBuilder;
 import pl.training.shop.commons.streotype.RestAdapter;
 import pl.training.shop.payments.ProcessPaymentUseCase;
 
 import static lombok.AccessLevel.PACKAGE;
+import static pl.training.shop.commons.rest.UriBuilder.requestUriWithId;
 
 @RequestMapping("payments")
 @RestAdapter
@@ -19,8 +21,12 @@ class ProcessPaymentUseCaseRestAdapter {
     private final ProcessPaymentUseCase processPaymentUseCase;
 
     @PostMapping
-    public ResponseEntity<PaymentDto> process(@RequestBody PaymentRequestDto paymentRequestDto) {
-
+    ResponseEntity<PaymentDto> process(@RequestBody PaymentRequestDto paymentRequestDto) {
+        var paymentRequest = mapper.toModel(paymentRequestDto);
+        var payment = processPaymentUseCase.process(paymentRequest);
+        var paymentDto = mapper.toDto(payment);
+        var locationUri = requestUriWithId(paymentDto.getId());
+        return ResponseEntity.created(locationUri).body(paymentDto);
     }
 
 }
