2b60c7ce49a291a22c8bab29f525e7a7e966ddd3
landrzejewski <landrzejewski.poczta@gmail.com>
2021-11-16 18:03:48 +0100

    day-2

--- shop/src/main/java/pl/training/shop/commons/rest/ExceptionDto.java
+++ shop/src/main/java/pl/training/shop/commons/rest/ExceptionDto.java
@@ -1,16 +1,12 @@
 package pl.training.shop.commons.rest;
 
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.Value;
 
 import java.time.LocalDateTime;
 
 @Value
-@RequiredArgsConstructor
 public class ExceptionDto {
 
-    @NonNull
     String description;
     LocalDateTime timestamp = LocalDateTime.now();
 
