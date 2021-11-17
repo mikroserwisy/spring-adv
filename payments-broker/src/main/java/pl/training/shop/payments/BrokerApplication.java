1a2f730f2afb74f17d992b070858225e3992c102
landrzejewski <landrzejewski.poczta@gmail.com>
2021-11-17 15:15:50 +0100

    day-3

---
+++ payments-broker/src/main/java/pl/training/shop/payments/BrokerApplication.java
@@ -0,0 +1,13 @@
+package pl.training.shop.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokerApplication.class, args);
+    }
+
+}
---
+++ payments-broker/src/test/java/pl/training/shop/payments/BrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.training.shop.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
