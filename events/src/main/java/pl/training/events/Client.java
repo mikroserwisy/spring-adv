e04bd095c7e26d99f6f2fc17480de3061867a919
landrzejewski <landrzejewski.poczta@gmail.com>
2021-11-19 15:32:36 +0100

    day-4

--- events/src/main/java/pl/training/events/Client.java
+++ events/src/main/java/pl/training/events/Client.java
@@ -1,6 +1,9 @@
 package pl.training.events;
 
+import lombok.extern.java.Log;
+import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 import reactor.core.publisher.Mono;
 
@@ -8,14 +11,27 @@ import java.net.URI;
 
 public class Client {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         var client = new ReactorNettyWebSocketClient();
-        client.execute(URI.create("ws://localhost:8080/events"), session -> {
-            var message = Mono.just(session.textMessage("Desktop client connected"));
-            return session.send(message)
-                    .thenMany(session.receive().map(WebSocketMessage::getPayloadAsText).log())
-                    .then();
-        }).block();
+        client.execute(URI.create("ws://localhost:8080/events"), new Handler()).subscribe();
+        Thread.sleep(10_000);
+    }
+
+}
+
+@Log
+class Handler implements WebSocketHandler {
+
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+        return session.receive()
+                .map(WebSocketMessage::getPayloadAsText)
+                .doOnNext(this::onMessage)
+                .then();
+    }
+
+    private void onMessage(String message) {
+        log.info(message);
     }
 
 }
